513e1cd7a475ae7a42eedab9c8d9f4d2a3b68662
Ibe Onyinye <ibetreasure999@gmail.com>
2022-09-11 15:21:34 -0700

    VARIABLES IN C

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,39 +1,21 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - prints the alphabet in lowercase, uses putchar
- *
- *   * Return: should return zero
- *
- *    */
-
+* main - prints the alphabet in lowercase, uses putchar
+* Return: should return zero
+*/
 int main(void)
-
 {
+	char ch;
 
-		char ch;
-
-
-
-			for (ch = 'a'; ch <= 'z'; ch++)
-
-					{
-
-								putchar(ch);
-
-									}
-
-				for (ch = 'A'; ch <= 'Z'; ch++)
-
-							putchar(ch);
-
-					putchar('\n');
-
-						return (0);
-
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	        putchar(ch);
+	putchar('\n');
+	return (0);
 }
 
 
