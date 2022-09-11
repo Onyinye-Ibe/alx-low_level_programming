8014049ed45d05102cdd8bd38978973931bda0f9
Ibe Onyinye <ibetreasure999@gmail.com>
2022-09-10 19:02:52 -0700

    VARIABLES IN C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main- prints the alphabet in lowercase,makes use of the putchar function
+ *
+ *   * Return: should return zero
+ *
+ *    */
+
+int main(void)
+
+{
+
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+
+	{
+
+		putchar(ch);
+
+	};
+
+	putchar('\n');
+
+	return (0);
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
+
+Privacy
+
+#include <stdio.h>
+
+/**
+ *
+ *  * main- prints the alphabet in lowercase,makes use of the putchar function
+ *
+ *   * Return: should return zero
+ *
+ *    */
+
+int main(void)
+
+{
+
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+
+	{
+
+		putchar(ch);
+
+	};
+
+	putchar('\n');
+
+	return (0);
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
+
+Privacy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
